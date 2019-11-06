13ed6c1f63cd752128d6229986f51b7e72bfc589
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 15:42:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,21 +36,21 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
+      },
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        },
+      },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
-     }
+      }
+  }
+  return[:dennis_ritchie][:languages]
 end
 
 def adding_matz
