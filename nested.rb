9336447e8c1859d360d02f7a42a6f242e222b4f2
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 15:41:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,11 +14,25 @@ def hopper
           :languages => ["C"]
       }
  	}
- 	return programmer_hash
+ 	return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	return hopper[:alan_kay][:known_for]
+	programmer_hash = {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+      },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+      },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+      }
+ 	}
+ 	return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
