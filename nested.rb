98f2fa81b54ce2c0b61ece67092bbfbe9eedab70
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 15:39:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -13,27 +13,12 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
       }
- 		}
+ 	}
+ 	return programmer_hash
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	return hopper[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
